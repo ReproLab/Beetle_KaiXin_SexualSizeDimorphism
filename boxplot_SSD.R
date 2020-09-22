f87bb909673b7673fb8379c231218a9108006593
tohkx <toh_kx@hotmail.com>
2020-09-22 13:41:51 +0800

    -

--- boxplot_SSD.R
+++ boxplot_SSD.R
@@ -8,7 +8,7 @@ library(plotrix)
 ##Load and check data
 getwd()
 setwd("/Users/kaixin/Beetle_KaiXin_SexualSizeDimorphism")
-data <- read.csv("SSDData2020.csv")
+data <- read.csv("SSDdata.csv")
 
 #Check Data
 head(data)
@@ -19,14 +19,17 @@ str(data)
 
 #Boxplot, mean and sd
 #pch is shape of point
-BSmean <- by(data$BodySize, data$Population, mean)
-BSsd <- by(data$BodySize, data$Population, sd)
+BSmean <- by(data$BS, data$Population, mean)
+BSsd <- by(data$BS, data$Population, sd)
 BSn <- summary(data$Population) # Sample sizes
 BSSE <- BSsd / sqrt(BSn)
 t.crit <- qt(0.95, BSn-1)
 int.width <- BSSE * t.crit
 plotCI(x=1:8, y=BSmean, xaxt="n", uiw=int.width, err='y',xlab="Population",ylim = c(3.2,4.2),
        ylab="Average protonum width (mm)\n(mean ± S.E.M.)", pch=15,
-       col=rep(c("#D55E00","#F0E442","black","#0072B2"), each=2), lty=rep(1:2, each=n))
+       col=rep(c("black","#F0E442","#0072B2", "#D55E00"), each=2), lty=rep(1:2, each=n))
+axis(1, at=1:8, labels=levels(data$Population), las=1)
+
+
 
 
